2851a828a3a61fbd316a24e13c9209b5358656d2
pal_user <arindam.roy@nttdata.com>
2018-08-28 17:47:50 -0600

    Movie Microservice URL

---
+++ applications/movie-service/src/main/java/org/superbiz/moviefun/movies/MovieServiceApplication.java
@@ -0,0 +1,14 @@
+package org.superbiz.moviefun.movies;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+public class MovieServiceApplication {
+    public static void main(String... args) {
+        SpringApplication.run(MovieServiceApplication.class, args);
+    }
+
+
+}
--- components/albums/src/main/java/org/superbiz/moviefun/albums/AlbumsUpdater.java
+++ components/albums/src/main/java/org/superbiz/moviefun/albums/AlbumsUpdater.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.superbiz.moviefun.CsvUtils;
 import org.superbiz.moviefun.blobstore.Blob;
 import org.superbiz.moviefun.blobstore.BlobStore;
 
